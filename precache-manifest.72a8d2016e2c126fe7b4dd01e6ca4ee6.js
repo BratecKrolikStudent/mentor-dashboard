ad24eb88a24668cd70721d2bb22e8e74a2ea82d2
Dmitryi Kunets <1mickeyfrost1@gmail.com>
2019-02-22 22:14:55 +0300

    add PUBLIC_URL

---
+++ precache-manifest.72a8d2016e2c126fe7b4dd01e6ca4ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c02c302165d52e2a07f",
+    "url": "./static/css/main.6325c9c9.chunk.css"
+  },
+  {
+    "revision": "1c02c302165d52e2a07f",
+    "url": "./static/js/main.1c02c302.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "1fd7358274fc6a99559d",
+    "url": "./static/js/2.1fd73582.chunk.js"
+  },
+  {
+    "revision": "3e9400ff103f9d46d2a3797a1052f46d",
+    "url": "./index.html"
+  }
+];
