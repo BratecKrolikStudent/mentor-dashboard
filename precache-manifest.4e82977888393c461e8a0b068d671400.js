01d68643fab5561137430c8be18a196e5b66a423
Dmitryi Kunets <1mickeyfrost1@gmail.com>
2019-02-22 21:56:48 +0300

    add PUBLIC_URL

---
+++ precache-manifest.4e82977888393c461e8a0b068d671400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c02c302165d52e2a07f",
+    "url": "mentor-dashboard/static/css/main.6325c9c9.chunk.css"
+  },
+  {
+    "revision": "1c02c302165d52e2a07f",
+    "url": "mentor-dashboard/static/js/main.1c02c302.chunk.js"
+  },
+  {
+    "revision": "8a85474b6f9d7e477f2a",
+    "url": "mentor-dashboard/static/js/runtime~main.8a85474b.js"
+  },
+  {
+    "revision": "1fd7358274fc6a99559d",
+    "url": "mentor-dashboard/static/js/2.1fd73582.chunk.js"
+  },
+  {
+    "revision": "21417532df71b12e6790a55603dd63a9",
+    "url": "mentor-dashboard/index.html"
+  }
+];
